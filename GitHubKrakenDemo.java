45a3e6620e2d737a8cd30c9ce3ffdd24a71e9223
suha-0116 <suham601@gmail.com>
2024-10-30 14:07:07 -0400

    Commit 2
    Configured threads

--- GitHubKrakenDemo.java
+++ GitHubKrakenDemo.java
@@ -1,17 +1,26 @@
 public class GitHubKrakenDemo implements Runnable
 {
-
+    long[] sums = new long[1000];
+    int count = 0;
     public static void main (String[] args)
     {
+
         Thread[] t = new Thread[1000];
         for (int i = 0; i < t.length; i++)
         {
             t[i] = new Thread(new GitHubKrakenDemo());
+            t[i].start();
         }
     }
 
     @Override
     public void run() {
-
+        long sum = 0;
+        for (int i = 1; i < 1000001; i++)
+        {
+            sum += i;
+        }
+        sums[count] = sum;
+        count++;
     }
 }
